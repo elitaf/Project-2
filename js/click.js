409fe7eab17f56fd6a83e6ae7e0f81fa063164b0
Nathaniel Garlock <ngarlock@sfu.ca>
2017-02-23 10:58:26 -0800

    Added a js folder and click.js file

---
+++ js/click.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+
+	$('#milk-tea').click(function(){
+		$('.images-1').addClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#green-tea').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').addClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#red-tea').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').addClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#black-tea').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').addClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#juice').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').addClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#soda').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').addClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#coffee').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').addClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#calpis').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').addClass("active");
+		$('.images-9').removeClass("active");
+	});
+
+	$('#mocktail').click(function(){
+		$('.images-1').removeClass("active");
+		$('.images-2').removeClass("active");
+		$('.images-3').removeClass("active");
+		$('.images-4').removeClass("active");
+		$('.images-5').removeClass("active");
+		$('.images-6').removeClass("active");
+		$('.images-7').removeClass("active");
+		$('.images-8').removeClass("active");
+		$('.images-9').addClass("active");
+	});
+});
