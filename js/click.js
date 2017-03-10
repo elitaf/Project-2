798e882c50fc79bd6a662a916292111d9361c91b
Nathaniel Garlock <ngarlock@sfu.ca>
2017-03-09 23:56:28 -0800

    replaced old click functions with proper click functions

--- js/click.js
+++ js/click.js
@@ -1,89 +1,86 @@
 $(document).ready(function() {
 
-	$('#milk-tea').click(function() {
+		$('#milk-tea').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-1').addClass("active");
 	});
 
-	$('#green-tea').click(function() {
+	$('#green-tea').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-2').addClass("active");
 	});
 
-	$('#red-tea').click(function() {
+	$('#red-tea').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-3').addClass("active");
 
 	});
 
-	$('#black-tea').click(function() {
+	$('#black-tea').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-4').addClass("active");
 
 	});
 
-	$('#juice').click(function() {
+	$('#juice').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-5').addClass("active");
 	});
 
-	$('#soda').click(function() {
+	$('#soda').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-6').addClass("active");
 	});
 
-	$('#coffee').click(function() {
+	$('#coffee').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-7').addClass("active");
 	});
 
-	$('#calpis').click(function() {
+	$('#calpis').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-8').addClass("active");
 	});
 
-	$('#mocktail').click(function() {
+	$('#mocktail').click(function(e) {
+		e.preventDefault();
 		$('#bbtpictures img').removeClass('active')
 		$('.images-9').addClass("active");
 	});
 
-	$('#pearls').click(function(){
+
+
+
+	$('#pearls').click(function(e) {
+		e.preventDefault();
+		$('#bbtpictures2 img').removeClass('active')
 		$('.images-10').addClass("active");
-		$('.images-11').removeClass("active");
-		$('.images-12').removeClass("active");
-		$('.images-13').removeClass("active");
-		$('.images-14').removeClass("active");
 	});
 
-	$('#sugar').click(function(){
-		$('.images-10').removeClass("active");
-		$('.images-11').addClass("active");
-		$('.images-12').removeClass("active");
-		$('.images-13').removeClass("active");
-		$('.images-14').removeClass("active");
-	});
 
-	$('#ice').click(function(){
-		$('.images-10').removeClass("active");
-		$('.images-11').removeClass("active");
+	$('#ice').click(function(e) {
+		e.preventDefault();
+		$('#bbtpictures2 img').removeClass('active')
 		$('.images-12').addClass("active");
-		$('.images-13').removeClass("active");
-		$('.images-14').removeClass("active");
 	});
 
-	$('#jelly').click(function(){
-		$('.images-10').removeClass("active");
-		$('.images-11').removeClass("active");
-		$('.images-12').removeClass("active");
+	$('#jelly').click(function(e) {
+		e.preventDefault();
+		$('#bbtpictures2 img').removeClass('active')
 		$('.images-13').addClass("active");
-		$('.images-14').removeClass("active");
 	});
 
-	$('#none').click(function(){
-		$('.images-10').removeClass("active");
-		$('.images-11').removeClass("active");
-		$('.images-12').removeClass("active");
-		$('.images-13').removeClass("active");
+	$('#none').click(function(e) {
+		e.preventDefault();
+		$('#bbtpictures2 img').removeClass('active')
 		$('.images-14').addClass("active");
 	});
 
